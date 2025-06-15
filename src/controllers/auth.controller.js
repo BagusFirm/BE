d9e1a0d9ff2ba85d6875fc2748fcda6a65fb0bae
safiratun <mc256d5x1807@student.devacademy.id>
2025-06-15 11:17:44 +0700

    revisi

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 const { getDB } = require('../config/db');
 const supabase = getDB();
+const Path = require('path');
+const Inert = require('@hapi/inert');
 
 const AuthService = require('../services/auth.service');
 
