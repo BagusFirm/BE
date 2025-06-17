73659f3af7c09d5ab00691c310f2b2d79fd9ad8b
safiratun <mc256d5x1807@student.devacademy.id>
2025-06-17 12:47:48 +0700

    revisi

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -152,7 +152,8 @@ const forgotPassword = async (payload, h) => {
 
   // Generate token & expiry 1 jam
   const token = crypto.randomBytes(32).toString('hex');
-const expiredAt = new Date(Date.now() + 6 * 60 * 60 * 1000).toISOString();
+const expiredAt = new Date(Date.now() + 3600 * 1000).toISOString(); // ✅
+
 
 
   // Simpan ke tabel password_resets
